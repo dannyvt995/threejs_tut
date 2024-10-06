eb1482457eb2c827c63f2b1dc88393f684dd6fbd
tuananhnvl <creatiflow.danny@gmail.com>
2024-10-06 20:12:51 +0700

    f

--- bvh_gensdf/RenderSDFLayerMaterial.js
+++ bvh_gensdf/RenderSDFLayerMaterial.js
@@ -1,4 +1,4 @@
-import { ShaderMaterial,Vector2 } from 'three';
+import { ShaderMaterial,Vector2,ClampToEdgeWrapping } from 'three';
 
 export class RenderSDFLayerMaterial extends ShaderMaterial {
 
@@ -17,9 +17,8 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 				sdfTex: { value: null },
 				layer: { value: 0 },
 				layers: { value: 0 },
-				uTexelSize : {value:new Vector2(1/20,1/20)}
+				uTexelSize : {value:new Vector2(0,0)}
 			},
-
 			vertexShader: /* glsl */`
 
 				varying vec2 vUv;
@@ -41,9 +40,7 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 				uniform float layer;
 				uniform float layers;
 				uniform vec2 uTexelSize;
-				vec3 gradientColor(float value) {
-					return vec3(0.5 + 0.5 * value, 0.5 - 0.5 * value, 0.0);
-				}
+				
 				void main() {
 
 					#if DISPLAY_GRID
@@ -55,13 +52,13 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 
 					float dist = texture( sdfTex, vec3( frac, zLayer ) ).r;
 				
-					gl_FragColor.rgb = dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
-					gl_FragColor.a = 1.0;
+					// gl_FragColor.rgb = dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
+					// gl_FragColor.a = 1.0;
 
 
 					 // Get gradient base sdf
-					float sdfCenter = texture(sdfTex, vec3(vUv, layer)).r;
-					vec3 texelSize = vec3(uTexelSize, 1.0 / float(textureSize(sdfTex, 0).z)); 
+				
+					vec3 texelSize = vec3(uTexelSize, 1./64.); 
 
 					float sdfRight = texture(sdfTex, vec3(frac + vec2(texelSize.x, 0.0), zLayer)).r;
 					float sdfLeft  = texture(sdfTex, vec3(frac - vec2(texelSize.x, 0.0), zLayer)).r;
@@ -76,46 +73,63 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 
 					float gradZ = (sdfFront - sdfBack) * 0.5;
 
-					vec3 gradient = vec3(gradX, gradY, gradZ);
+					vec3 gradient = vec3(gradX, gradY, gradZ) ;
 
 					gradient = normalize(gradient);
 
 					// out clr
 					// up color gradient
-					float gradientMagnitude = length(gradient);
+			
+
+					vec3 outGradient2 = vec3(gradient * 0.5 + 0.5);
+					outGradient2.r = outGradient2.r * 0.7; // Giảm màu đỏ xuống 50%
+					outGradient2.g = outGradient2.g * 1.2; // Tăng màu xanh lá cây lên 150%
+					outGradient2.b = outGradient2.b * 1.2; // Tăng màu xanh dương lên 150%
+
+					// Đảm bảo không vượt quá 1.0
+					outGradient2 = pow(outGradient2, vec3(1.5)); 
+					outGradient2 = clamp(outGradient2, 0.0, 1.0);
+
+					gl_FragColor = vec4(outGradient2, 1.);
+					// gl_FragColor = vec4(outGradient2,1.- dist);
+					// gl_FragColor = vec4(step(.2,vec3(dist)), dist + .5);
+
+
+
+
+					vec3 infoOrigin = texture( sdfTex, vec3( frac, zLayer ) ).rgb;
+					gl_FragColor.rgb = step(.02,infoOrigin);
+					gl_FragColor.rgb = infoOrigin;
+					gl_FragColor.a = 1.0;
+
+
+
+
 
 				
-					vec3 outGradient = vec3(0.0);
-					if (gradientMagnitude > 0.0) {
-						
-						outGradient = gradient * 0.5 + 0.5; // Chuyển đổi [-1, 1] thành [0, 1]
-						
-						outGradient = pow(outGradient, vec3(2.0)); 
-						outGradient = mix(vec3(0.0), outGradient, 0.8); 
-					}
-					vec3 distanceOut =  dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
-				
-					vec3 combine2 = outGradient * 1. + distanceOut * 0.;
-		
-
-					gl_FragColor = vec4(combine2, dist + .5);
-				//	gl_FragColor = vec4(step(.1,vec3(dist)), dist + .5);
 					#else
 
-					float dist = texture( sdfTex, vec3( vUv, layer ) ).r;
+				
 
 					 // Get gradient base sdf
-					float sdfCenter = texture(sdfTex, vec3(vUv, layer)).r;
-					vec3 texelSize = vec3(uTexelSize, 1.0 / float(textureSize(sdfTex, 0).z)); 
+		
+					vec3 texelSize = vec3(uTexelSize, 1./64.); 
 
-					float sdfRight = texture(sdfTex, vec3(vUv + vec2(texelSize.x, 0.0), layer)).r;
-					float sdfLeft  = texture(sdfTex, vec3(vUv - vec2(texelSize.x, 0.0), layer)).r;
-					float sdfUp    = texture(sdfTex, vec3(vUv + vec2(0.0, texelSize.y), layer)).r;
-					float sdfDown  = texture(sdfTex, vec3(vUv - vec2(0.0, texelSize.y), layer)).r;
+
+					vec2 clampedUV1 = clamp(vUv + vec2(texelSize.x, 0.0), 0.0, 1.);
+					vec2 clampedUV2 = clamp(vUv - vec2(texelSize.x, 0.0), 0.0, 1.);
+					vec2 clampedUV3 = clamp(vUv + vec2(0.0, texelSize.y), 0.0, 1.);
+					vec2 clampedUV4 = clamp(vUv - vec2(0.0, texelSize.y), 0.0, 1.);
+
+					float sdfRight = texture(sdfTex, vec3(clampedUV1, layer)).r;
+					float sdfLeft  = texture(sdfTex, vec3(clampedUV2, layer)).r;
+					float sdfUp    = texture(sdfTex, vec3(clampedUV3, layer)).r;
+					float sdfDown  = texture(sdfTex, vec3(clampedUV4, layer)).r;
 
 					float gradX = (sdfRight - sdfLeft) * 0.5;
 					float gradY = (sdfUp - sdfDown) * 0.5;
 
+					
 					float sdfFront = texture(sdfTex, vec3(vUv, layer + texelSize.z)).r;
 					float sdfBack  = texture(sdfTex, vec3(vUv, layer - texelSize.z)).r;
 
@@ -125,26 +139,28 @@ export class RenderSDFLayerMaterial extends ShaderMaterial {
 
 					gradient = normalize(gradient);
 
-					// out clr
-					// up color gradient
-					float gradientMagnitude = length(gradient);
-
+					
 				
-					vec3 outGradient = vec3(0.0);
-					if (gradientMagnitude > 0.0) {
-						
-						outGradient = gradient * 0.5 + 0.5; // Chuyển đổi [-1, 1] thành [0, 1]
-						
-						outGradient = pow(outGradient, vec3(2.0)); 
-						outGradient = mix(vec3(0.0), outGradient, 0.8); 
-					}
+					vec3 outGradient2 = vec3(gradient * 0.5 + 0.5);
+					outGradient2.r = outGradient2.r * 0.6; // Giảm màu đỏ xuống 50%
+					outGradient2.g = outGradient2.g * 1.1; // Tăng màu xanh lá cây lên 150%
+					outGradient2.b = outGradient2.b * 1.1; // Tăng màu xanh dương lên 150%
+
+					// Đảm bảo không vượt quá 1.0
+					outGradient2 = pow(outGradient2, vec3(1.5)); 
+					outGradient2 = clamp(outGradient2, 0.0, 1.0);
+
+					gl_FragColor = vec4(outGradient2, 1.);
+				
+
+
+
+					float dist = texture( sdfTex, vec3( vUv, layer ) ).r;
 					vec3 distanceOut = 	 dist > 0.0 ? vec3( 0, dist, 0 ) : vec3( - dist, 0, 0 );
-				
-					vec3 combine2 = outGradient * 0. + distanceOut * 1.;
-		
-
-					gl_FragColor = vec4(combine2, 1.0);
-
+			
+					//gl_FragColor = vec4(distanceOut,1.);
+	
+	
 					#endif
 
 					#include <colorspace_fragment>
