1d63243fe466d7f209afa546c3600fb4e187bc45
tuananhnvl <creatiflow.danny@gmail.com>
2024-10-29 23:32:29 +0700

    new

---
+++ garrettkjohnson/BatchedStandardMaterial.js
@@ -0,0 +1,101 @@
+import { MeshStandardMaterial } from 'three';
+import { BatchedPropertiesTexture } from './BatchedPropertiesTexture.js';
+
+const properties = {
+    diffuse: 'vec3',
+    emissive: 'vec3',
+    metalness: 'float',
+    roughness: 'float',
+};
+
+export class BatchedStandardMaterial extends MeshStandardMaterial {
+
+    constructor( params, geometryCount, propertiesList = null ) {
+
+        super( params );
+
+        let props = { ...properties };
+        if ( propertiesList ) {
+
+            for ( const key in props ) {
+
+                if ( ! propertiesList.includes( key ) ) {
+
+                    delete props[ key ];
+
+                }
+
+            }
+
+        }
+
+        const propertiesTex = new BatchedPropertiesTexture( props, geometryCount );
+        this.propertiesTex = propertiesTex;
+
+        this.onBeforeCompile = ( parameters, renderer ) => {
+
+            if ( Object.keys( props ).length === 0 ) {
+
+                return;
+
+            }
+
+            parameters.uniforms.propertiesTex = { value: propertiesTex };
+
+            parameters.vertexShader = parameters
+                .vertexShader
+                .replace(
+                    'void main() {',
+                    `
+                        varying float vBatchId;
+                        void main() {
+
+                            // add 0.5 to the value to avoid floating error that may cause flickering
+                            vBatchId = batchId + 0.5;
+                            
+                    `
+                )
+                .replace(
+                    '#include <begin_vertex>',`
+
+                    vec3 poss = position;
+                poss.x += sin(poss.y + batchId * 5.)  * 1. ;
+                   poss.y += cos(poss.x + batchId * 5.)  * 1. ;
+                    vec3 transformed = poss;
+           
+           
+                    
+					
+                    `)
+            console.log( parameters.vertexShader)
+            parameters.fragmentShader = parameters
+                .fragmentShader
+                .replace(
+                    'void main() {',
+                    `
+                        uniform highp sampler2D propertiesTex;
+                        varying float vBatchId;
+                        void main() {
+  
+                            ${ propertiesTex.getGlsl() }
+                    `
+                );
+
+        };
+
+    }
+
+    setValue( ...args ) {
+
+        this.propertiesTex.setValue( ...args );
+        
+    }
+
+    dispose() {
+
+        super.dispose();
+        this.propertiesTex.dispose();
+
+    }
+
+}
